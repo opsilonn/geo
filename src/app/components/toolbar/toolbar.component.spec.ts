e1f374e206714425a6fd267b6b3f1de1b3ef8e9d
opsilonn <huguesbegeot@gmail.com>
2023-08-20 21:50:44 +0200

    Ajoute l'interaction au DialogCreatePartyComponent

--- src/app/components/toolbar/toolbar.component.spec.ts
+++ src/app/components/toolbar/toolbar.component.spec.ts
@@ -1,14 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
 import { AppModule } from 'app/app.module';
 import { ToolbarComponent } from 'app/components/toolbar/toolbar.component';
 import { MockBuilder } from 'ng-mocks';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
+  let dialog: jasmine.SpyObj<MatDialog>;
 
   beforeEach(() => MockBuilder(ToolbarComponent, AppModule));
 
   beforeEach(() => {
+    dialog = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
+
     const fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -16,5 +20,16 @@ describe('ToolbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#openDialog Quand, alors', async () => {
+        // Given
+        spyOn(dialog, 'open');
+
+        // When
+        await component.openDialog();
+    
+        // Then
+        expect(dialog.open).toHaveBeenCalled();
   });
 });
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCreatePartyComponent } from 'app/components/dialog-create-party/dialog-create-party.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class ToolbarComponent {
 
+  constructor(public dialog: MatDialog) {}
+
+  /** Ouvre le dialog pour configurer une nouvelle partie */
+  public async openDialog(): Promise<void> {
+    this.dialog.open(DialogCreatePartyComponent);
+  }
 }
